eb70f856f9b9013cf612f2691d27afcbf7e56314
git-dev-connect <gitdevconnect@gmail.com>
2024-08-11 16:06:22 +0530

    Initial commit

---
+++ src/main/java/com/shopnow/ShopNowApp/ShopNowAppApplication.java
@@ -0,0 +1,13 @@
+package com.shopnow.ShopNowApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopNowAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopnow/ShopNowApp/ShopNowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopnow.ShopNowApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
